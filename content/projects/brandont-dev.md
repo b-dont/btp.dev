e4c9f130426d9910b885c9349a5f740e9366fbba
Brandon Phillips <btp@brandont.dev>
2023-04-02 10:12:49 -0600

    Added website project, adjusted new-project script.

---
+++ content/projects/brandont-dev.md
@@ -0,0 +1,12 @@
++++
+title = "brandont.dev"
+description = "Personal blog and website, built with the Zola static site generator."
+
+[taxonomies]
+tags = ["zola", "webdev"]
+
+[extra]
+codeberg = "https://codeberg.org/brandont/brandont.dev"
+github = "https://github.com/b-dont/brandont.dev"
+gitlab = "https://gitlab.com/bdont/brandont.dev"
++++
--- scripts/new-project.sh
+++ scripts/new-project.sh
@@ -2,5 +2,5 @@
 
 set -euf
 
-printf "+++\ntitle = \"\"\ndescription = \"\"\n\n[taxonomies]\ntags = [\"\"]\n\n[extra]\ncodeberg = \"\"\ngithub = \"\"\ngitlab = \"\"\n+++\n" \
+printf "+++\ntitle = \"\"\ndescription = \"\"\n\n[taxonomies]\ntags = [\"\"]\n\n[extra]\ncodeberg = \"https://codeberg.org/brandont/\"\ngithub = \"https://github.com/b-dont/\"\ngitlab = \"https://gitlab.com/bdont/\"\n+++\n" \
 >> $1
