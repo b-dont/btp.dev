c7fd37d4a10be61411dfc7fe6a7448b542b71021
Brandon Phillips <btp@brandont.dev>
2023-06-23 07:37:20 -0600

    Reverting

--- scripts/new-project.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-set -euf
-
-printf "+++\ntitle = \"\"\ndescription = \"\"\n\n[taxonomies]\ntags = [\"\"]\n\n[extra]\ncodeberg = \"https://codeberg.org/brandont/\"\ngithub = \"https://github.com/b-dont/\"\ngitlab = \"https://gitlab.com/bdont/\"\n+++\n" \
->> $1
