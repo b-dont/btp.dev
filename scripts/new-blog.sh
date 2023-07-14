9f180bb15b981dfdcc3c03a1c405b00c5aae3a0e
Brandon Phillips <btp@brandont.dev>
2023-07-14 07:33:44 -0600

    New blog post

--- scripts/new-blog.sh
+++ scripts/new-blog.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 set -euf
+date=$(date '+%Y-%m-%d') 
 
-printf "+++\ntitle = \"\"\ndescription = \"\"\ntemplate = \"blog_post.html\"\ndate = \"\"\ndraft = true\n[taxonomies]\ntags = []\n+++\n" >> $1
+printf "+++\ntitle = \"\"\ndescription = \"\"\ntemplate = \"blog_post.html\"\ndate = \"$date\"\ndraft = true\n[taxonomies]\ntags = []\n+++\n" >> $1
 
