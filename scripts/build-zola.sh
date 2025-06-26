0d458d785106ce8d389b666a24fd964cf9b9a385
Brandon Phillips <btp@brandont.dev>
2025-06-26 10:58:24 -0600

    Updating post-receive scripts

--- scripts/build-zola.sh
+++ scripts/build-zola.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-podman run -it --rm --network=host \
+docker run -it --rm --network=host \
 	-v $PWD:/app \
 	--workdir /app \
 	ghcr.io/getzola/zola:v0.17.2 --config config/zola.config.toml build
