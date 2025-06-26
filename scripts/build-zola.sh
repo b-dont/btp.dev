c290e24b2c3c1d1093435ecc7e2d96902e9a5ba2
Brandon Phillips <btp@brandont.dev>
2025-06-26 14:48:27 -0600

    Updating bare repo hook

--- scripts/build-zola.sh
+++ scripts/build-zola.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 docker run -u "$(id -u):$(id -g)" \
-	-v /var/www/brandont.dev-prod/src:/app \
+	-v "$PWD"/src:/app \
 	--workdir /app \
 	ghcr.io/getzola/zola:v0.20.0 --config zola.toml build
