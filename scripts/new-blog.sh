7dbed3209b3380c3d3e3b2beb2e104030f2e2ea8
Brandon Phillips <btp@brandont.dev>
2025-07-06 09:14:48 -0600

    Updating new blog script

--- scripts/new-blog.sh
+++ scripts/new-blog.sh
@@ -2,6 +2,6 @@
 
 set -euf
 date=$(date '+%Y-%m-%d') 
+title=$(echo "$1" | tr ' ' '-')
 
-printf "+++\ntitle = \"\"\ndescription = \"\"\nauthors = [\"Brandon Phillips\"]\ntemplate = \"blog_post.html\"\ndate = \"$date\"\ndraft = true\n[taxonomies]\ntags = []\n+++\n" >> $1.md
-
+printf '+++\n%s = \"\"\ndescription = \"\"\nauthors = [\"Brandon Phillips\"]\ntemplate = \"blog_post.html\"\ndate = \"%s\"\ndraft = true\n[taxonomies]\ntags = []\n+++\n' "$1" "$date" >> "$title".md
