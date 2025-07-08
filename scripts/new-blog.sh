6e2962ea6446f201e23e44dbabee781aff4dab85
Brandon Phillips <btp@brandont.dev>
2025-07-07 18:46:51 -0600

    New blog post

--- scripts/new-blog.sh
+++ scripts/new-blog.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 set -euf
-date=$(date '+%Y-%m-%d') 
-title=$(echo "$1" | tr ' ' '-')
 
-printf '+++\n%s = \"\"\ndescription = \"\"\nauthors = [\"Brandon Phillips\"]\ntemplate = \"blog_post.html\"\ndate = \"%s\"\ndraft = true\n[taxonomies]\ntags = []\n+++\n' "$1" "$date" >> "$title".md
+date=$(date '+%Y-%m-%d') 
+title=$1
+declare -l title
+title=${title// /-}
+
+printf '+++\ntitle = \"%s\"\ndescription = \"\"\nauthors = [\"Brandon Phillips\"]\ntemplate = \"blog_post.html\"\ndate = \"%s\"\ndraft = true\n[taxonomies]\ntags = []\n+++\n' "$1" "$date" >> "$title".md
